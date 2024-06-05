8d5e99450cd2bcaaa02d44f9c79aa0ead0b8653f
Waqas Ahmed <wa02263@gmail.com>
2024-06-05 11:54:48 +0500

    same col/row enctypted

--- app/src/main/java/com/example/PlayfairCipher.kt
+++ app/src/main/java/com/example/PlayfairCipher.kt
@@ -1,5 +1,7 @@
 package com.example
 
+import android.util.Log
+
 
 class Solution() {
 
@@ -10,7 +12,7 @@ class Solution() {
     val matrix = mutableListOf<MutableList<Char>>()
 
     //    val text = "HELLO"
-    val text = "ME"
+    val text = "MCELU"
     val chunkedText = mutableListOf<String>()
     val encryptedText = mutableListOf<String>()
 
@@ -44,25 +46,26 @@ class Solution() {
 
             }
         }
-        print(matrix)
+//        print(matrix)
         var i = 0
         while (i < text.length) {
-            if (i + 1 < text.length && text[i] != text[i + 1]) {
+            i += if (i + 1 < text.length && text[i] != text[i + 1]) {
                 chunkedText.add(text.substring(i, i + 2))
-                i += 2
-            } else if (text[i] == text[i + 1]) {
+                2
+            } else if (i + 1 < text.length && text[i] == text[i + 1]) {
                 chunkedText.add(text.substring(i, i + 1) + BOGUS_LETTER)
-                i += 1
+                1
 
             } else {
                 chunkedText.add(text.substring(i, i + 1) + BOGUS_LETTER)
-                i += 2
+                2
             }
         }
+        println("chnked text $chunkedText")
 
         //encryption logic
         for (pairs in chunkedText) {
-            if(pairInSameRowOrCol(pair = pairs))continue
+            if (pairInSameRowOrCol(pair = pairs)) continue
             for (k in 0..matrix.size - 1) {
 
                 for (matrixChar in matrix[k]) {
@@ -73,13 +76,30 @@ class Solution() {
         }
     }
 
+
+    //func to chk whether pair in same row/col
     fun pairInSameRowOrCol(pair: String): Boolean {
 
+        var encryptedPair = ""
         //check whether in same row
         for (j in 0..matrix.size - 1) {
             if (pair[0] in matrix[j] && pair[1] in matrix[j]) {
-//                encryptedText.add(matrix[j][ matrix[j].indexOf()])
-                print(true)
+                if (matrix[j].indexOf(pair[0]) != matrix[j].size - 1) {
+                    encryptedPair = matrix[j][matrix[j].indexOf(pair[0]) + 1].toString()
+                    println("in first if $encryptedPair")
+                    println("index in first if ${matrix[j].indexOf(pair[0])}")
+                } else {
+                    encryptedPair = matrix[j][0].toString()
+
+                }
+                encryptedPair = if (matrix[j].indexOf(pair[1]) != matrix[j].size - 1) {
+                    "$encryptedPair${matrix[j][matrix[j].indexOf(pair[1]) + 1]}"
+                } else {
+                    "$encryptedPair${matrix[j][0]}"
+
+                }
+                encryptedText.add(encryptedPair)
+                encryptedPair = ""
                 return true
             }
 
@@ -87,19 +107,29 @@ class Solution() {
 
         //check whether in same col
         var totalCharFound = 0
-        for (i in 0 until matrix.size ) {
+        for (i in 0 until matrix.size) {
             for (j in 0 until matrix.size) {
                 if (pair[0] == matrix[j][i] || pair[1] == matrix[j][i]) {
                     println("pairs are ${pair[0]}, ${pair[1]} and char is ${matrix[j][i]}")
                     totalCharFound += 1
+                    if (j != matrix[i].size - 1) {
+                        encryptedPair = "$encryptedPair${matrix[j + 1][i]}"
+                        println("in firKx in first if ${matrix[j].indexOf(pair[0])}")
+                    } else {
+                        encryptedPair = "$encryptedPair${matrix[0][0]}"
+
+                    }
                     if (totalCharFound == 2) {
-                        println("found in col")
+                        encryptedText.add(encryptedPair)
+                        encryptedPair = ""
                         return true
                     }
 
                 }
             }
             totalCharFound = 0
+            encryptedPair = ""
+
         }
         return false
     }
